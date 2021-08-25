fb5178096b016b34f16787e545f11b2f460d821a
Nihat <senius.33@gmail.com>
2021-08-26 00:21:21 +0300

    Edit firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+export default config;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,11 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import config from "./config";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
-
-// db.enablePersistence().catch((error) => {
-//   console.log(`Firebase persistence error ${error.code}`);
-// });
 
 export default firebase;
