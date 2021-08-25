cc220d6093671f215b7dd03b2a74ed1c50cc7362
Nihat <senius.33@gmail.com>
2021-08-26 00:31:13 +0300

    Edit test component

--- src/__tests__/components/ImageSection.test.js
+++ src/__tests__/components/ImageSection.test.js
@@ -3,6 +3,7 @@ import React from "react";
 import ImageSection from "../../components/imageSection/ImageSection";
 
 it("expect to render ImageSection component", () => {
+  console.log("Testing");
   const wrapper = shallow(<ImageSection />);
   expect(wrapper.length).toBe(1);
   expect(wrapper).toMatchSnapshot();
